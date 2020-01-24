fd6b7d7ab3427da5670e6fddbaec62b912b6515f
Andrew Weiss <wandrew8@gmail.com>
2020-01-23 19:38:58 -0800

    error handling added to fetch in actionTypes redux

--- src/redux/ActionCreator.js
+++ src/redux/ActionCreator.js
@@ -16,8 +16,22 @@ export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
 
     return fetch(baseUrl + 'campsites')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`)
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            const errMess = new Error(error.message);
+            throw errMess
+        })
         .then(response => response.json())
         .then(campsites => dispatch(addCampsites(campsites)))
+        .catch(error => dispatch(campsitesFailed(error.message)))
 };
 
 export const campsitesLoading = () => ({
@@ -47,18 +61,43 @@ export const addComments = comments => ({
 
 export const fetchComments = () => dispatch => {
     return fetch(baseUrl + 'comments')
+        .then(response => {
+            if (response.ok) {
+                return response
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            const errMess = new Error(error.message);
+            throw errMess;
+        })
         .then(response => response.json())
         .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)))
 }
 
 export const fetchPromotions = () => dispatch => {
-
     dispatch(promotionsLoading());
-
     return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if(response.ok) {
+                return response;
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            const errMess = new Error(error.message);
+            throw errMess;
+        })
         .then(response => response.json())
         .then(promotions => dispatch(addPromotions(promotions)))
-};
+        .catch(error => dispatch(promotionsFailed(error.message)));
+    };
 
 export const promotionsLoading = () => ({
     type: ActionTypes.PROMOTIONS_LOADING
